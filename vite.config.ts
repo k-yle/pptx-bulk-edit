d22ca5cda884e7938167156b629d8eed056de713
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-07-16 23:21:22 +1200

    fix absolute import paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "pptx-bulk-edit",
   plugins: [react()],
   server: {
     host: "127.0.0.1",
